bd85fef5fb316745808dea6889816b4081ec91bd
Aditya Agarwal <adi790u@gmail.com>
2023-08-11 21:54:17 +0530

    Deploy

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Set your base URL here
+  baseURL: 'https://co-compiler-backend.onrender.com/api/v1', // Set your base URL here
   headers: {
     'Content-Type': 'application/json', // Set common headers here
     // Add other headers as needed
